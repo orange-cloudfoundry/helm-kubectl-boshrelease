8e1600e8cd4b299127366100e5331a8fde2ae9ef
obeyler <olivier.beyler@orange.com>
2018-06-04 15:36:45 +0000

    initial

---
+++ addblob.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#-------------------------
+
+HELM_VERSION=2.9.1
+KUBECTL_VERSION=1.10.3
+
+#-------------------------
+
+HELM_FILE=helm-v${HELM_VERSION}-linux-amd64.tar.gz
+
+wget https://storage.googleapis.com/kubernetes-helm/${HELM_FILE}
+wget https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL_VERSION}/bin/linux/amd64/kubectl
+
+bosh add-blob ${HELM_FILE} helm/${HELM_VERSION}/helm.tar.gz
+bosh add-blob kubectl kubectl/${KUBECTL_VERSION}/kubectl
+
+rm ${HELM_FILE}
+rm kubectl

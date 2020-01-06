0fac22b3192484bf3a0b26f0c786604907ae96fc
obeyler <olivier.beyler@orange.com>
2020-01-06 09:32:26 +0000

    bump to helm v3 and add namespace creation

--- addblob.sh
+++ addblob.sh
@@ -1,19 +1,14 @@
-#!/bin/sh
-
-#-------------------------
-
-HELM_VERSION=3.0.2
-KUBECTL_VERSION=1.15.4
-
-#-------------------------
+#!/bin/bash
+source ./set_version.sh
+bosh add-blob set_version.sh set_version.sh
 
 HELM_FILE=helm-v${HELM_VERSION}-linux-amd64.tar.gz
 
-wget https://storage.googleapis.com/kubernetes-helm/${HELM_FILE}
+wget https://get.helm.sh/${HELM_FILE}
 wget https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL_VERSION}/bin/linux/amd64/kubectl
 
-bosh add-blob ${HELM_FILE} helm/${HELM_VERSION}/helm.tar.gz
-bosh add-blob kubectl kubectl/${KUBECTL_VERSION}/kubectl
+bosh add-blob ${HELM_FILE} helm/helm.tar.gz
+bosh add-blob kubectl kubectl/kubectl
 
 rm ${HELM_FILE}
 rm kubectl
---
+++ set_version.sh
@@ -0,0 +1,5 @@
+export HELM_VERSION=3.0.2
+export KUBECTL_VERSION=1.15.4
+
+echo "helm version:"$HELM_VERSION
+echo "kubectl version:"$KUBECTL_VERSION

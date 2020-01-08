e9d32cdf57995782eb79a69bb22141f3ddd8752d
olivier beyler <olivier.beyler@orange.com>
2020-01-08 14:12:12 +0100

    add worker-label

--- addblob.sh
+++ addblob.sh
@@ -6,9 +6,12 @@ HELM_FILE=helm-v${HELM_VERSION}-linux-amd64.tar.gz
 
 wget https://get.helm.sh/${HELM_FILE}
 wget https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL_VERSION}/bin/linux/amd64/kubectl
+wget https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
 
+bosh add-blob jq-linux64 jq/jq-linux64
 bosh add-blob ${HELM_FILE} helm/helm.tar.gz
 bosh add-blob kubectl kubectl/kubectl
 
 rm ${HELM_FILE}
 rm kubectl
+rm jq-linux64

0ef4d9a5fd8b9432ca513dcb693456e219bde7d4
olivier beyler <olivier.beyler@orange.com>
2020-01-31 15:07:28 +0100

    add label and annotation on namespaces

--- jobs/action/templates/bin/kubectl_namespace.rb
+++ jobs/action/templates/bin/kubectl_namespace.rb
@@ -4,17 +4,24 @@
 def do_create_namespace(namespace)
   name = namespace['name']
   annotations = namespace['annotations']
+  labels = namespace['labels']
   filename="/tmp/ns_#{name}.yml"
 
   File.open(filename, 'w+') do |f|
     f.puts("apiVersion: v1")
     f.puts("kind: Namespace")
     f.puts("metadata:")
+    f.puts("  name: #{name}")
     f.puts("  labels:")
     f.puts("    name: #{name}")
-    f.puts("  name: #{name}")
+    if labels != nil
+      labels.each{ |label|
+        f.puts("    #{label['name']}: #{label['value']}")
+      }
+    end
 
     if annotations != nil
+      f.puts("  anotations: ")
       annotations.each{ |annotation|
         f.puts("    #{annotation['name']}: #{annotation['value']}")
       }

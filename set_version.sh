4bfe7674702a9bad0e9e1e7731642b40d03a85c9
olivier beyler <olivier.beyler@orange.com>
2020-11-25 12:41:05 +0100

    bump version helm 3.4.1 bump kubectl version 1.18.11

--- set_version.sh
+++ set_version.sh
@@ -1,5 +1,5 @@
-export HELM_VERSION=3.3.0
-export KUBECTL_VERSION=1.18.5
+export HELM_VERSION=3.4.1
+export KUBECTL_VERSION=1.18.11
 
 echo "helm version:"$HELM_VERSION
 echo "kubectl version:"$KUBECTL_VERSION

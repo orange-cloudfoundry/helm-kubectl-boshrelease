67ca36fbfacff5f9fb866d8185592270c2a02b8e
Olivier ORAND <olivier.orand@orange.com>
2021-04-06 14:40:02 +0200

    create release 36

--- create-final-release.sh
+++ create-final-release.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+set -e
 bosh create-release --final
 
 git status
+git add  releases/helm-kubectl/*
+git add  .final_builds/*
+
 
 echo "Ensure S3 bucket are publicly available !"
 # see https://docs.aws.amazon.com/cli/latest/reference/s3api/put-bucket-policy.html
+echo "Create a github release"

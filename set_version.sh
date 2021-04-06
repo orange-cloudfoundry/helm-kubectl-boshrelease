53a537baa3d2811d73081e96028a2b9755b8c562
Olivier ORAND <olivier.orand@orange.com>
2021-04-06 19:12:40 +0200

    bump helm, kubetcl and kustomize to latest version
    Version:
        HELM_VERSION=3.5.3
        KUBECTL_VERSION=1.19.9
        KUSTOMIZE_VERSION=4.0.5

--- set_version.sh
+++ set_version.sh
@@ -1,6 +1,6 @@
-export HELM_VERSION=3.5.0
-export KUBECTL_VERSION=1.19.5
-export KUSTOMIZE_VERSION=3.9.2
+export HELM_VERSION=3.5.3
+export KUBECTL_VERSION=1.19.9 # Warning ensure kubectl version is never greater than our k8s version
+export KUSTOMIZE_VERSION=4.0.5
 
 echo "helm version:"$HELM_VERSION
 echo "kubectl version:"$KUBECTL_VERSION

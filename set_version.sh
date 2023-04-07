f182cabdc56116063108b686b17318ba3ad5cf24
Pierre Oblin <bosh@poblin-cli-0.poblin-cli.00-bastion-access.svc.cluster.local>
2023-04-07 17:33:16 +0000

    bump helm 3.11.2, kubectl 1.24.9, kustomize 4.5

--- set_version.sh
+++ set_version.sh
@@ -1,6 +1,6 @@
-export HELM_VERSION=3.5.3
-export KUBECTL_VERSION=1.19.9 # Warning ensure kubectl version is never greater than our k8s version
-export KUSTOMIZE_VERSION=4.0.5
+export HELM_VERSION=3.11.2
+export KUBECTL_VERSION=1.24.9 # Warning ensure kubectl version is never greater than our k8s version
+export KUSTOMIZE_VERSION=4.5.7
 
 echo "helm version:"$HELM_VERSION
 echo "kubectl version:"$KUBECTL_VERSION

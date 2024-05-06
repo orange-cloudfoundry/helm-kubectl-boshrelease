51d70ba81b251ff74174a6a28a43fd7ff14a6023
Olivier ORAND <olivier.orand@orange.com>
2024-05-06 16:05:07 +0000

    major: dummy commit to create a major release
    Reminder thoses paths are ignored:
          - '.github/**'
          - .gitignore
          - CONTRIBUTING.md
          - README.md
          - renovate.json5
          - run-renovate-locally.bash

--- set_version.sh
+++ set_version.sh
@@ -2,6 +2,5 @@ echo "This script is useless with renovate"
 #export HELM_VERSION=3.11.2
 #export KUBECTL_VERSION=1.24.9 # Warning ensure kubectl version is never greater than our k8s version
 #export KUSTOMIZE_VERSION=4.5.7
-#
 #echo "helm version:"$HELM_VERSION
 #echo "kubectl version:"$KUBECTL_VERSION
